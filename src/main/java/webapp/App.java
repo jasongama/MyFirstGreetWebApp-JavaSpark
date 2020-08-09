3d49349a4144ec734d5c5dc7e657d45d0fadbcda
JasonGama <Jasongama967@gmail.com>
2020-08-09 15:02:04 +0200

    my first java webapp

--- src/main/java/webapp/App.java
+++ src/main/java/webapp/App.java
@@ -1,5 +1,7 @@
 package webapp;
 
+//import org.jdbi.v3.core.Handle;
+//import org.jdbi.v3.core.Jdbi;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
@@ -12,13 +14,44 @@ public class App {
 
     public static void main(String[] args) {
 
-        getHerokuAssignedPort();
+        port(getHerokuAssignedPort());
 
         Map<String, Object> map = new HashMap<>();
         List<String> greetings = new ArrayList<>();
+//        String dbDiskURL = "jdbc:h2:file:./greetdb";
+//        String dbMemoryURL = "jdbc:h2:mem:greetdb";
+//
+//        Jdbi jdbi = Jdbi.create(dbDiskURL, "sa", "");
+//
+//        // get a handle to the database
+//        Handle handle = jdbi.open();
+//
+//       // create the table if needed
+//        handle.execute("create table if not exists greet ( id integer identity, name varchar(50), counter int )");
+//
+//        // add a user to the database
+//        handle.execute("insert into greet (name) values (?)", username);
+//
+//// get all the usernames from the database
+//        List<String> names = handle.createQuery("select name from greet")
+//                .mapTo(String.class)
+//                .list();
+//
+//// get a count of all the users in the database
+//        int count = handle.select("select count(*) from greet")
+//                .mapTo(int.class)
+//                .findOnly();
+//
+//
+//// check if a user exists
+//        int counts = handle.select("select count(*) from greet where name = ?", userName)
+//                .mapTo(int.class)
+//                .findOnly();
 
+//         get("greeted/:username", (req, res) ->new ModelAndView(map, "hello.handlebars"),
+//                 new HandlebarsTemplateEngine());
 
-        get("/greet", (req, res) -> new ModelAndView(map, "hello.handlebars"),
+        get("/", (req, res) -> new ModelAndView(map, "hello.handlebars"),
                 new HandlebarsTemplateEngine());
 
 
@@ -66,11 +99,11 @@ public class App {
                 map.put("greeting", greet);
                 map.put("count", count);
                 map.put("language", lang);
-                map.put("user", greetings);
+                map.put("userName", greetings);
             } else {
                 map.put("greeting", "Enter name and select language.");
             }
-            res.redirect("/greet");
+            res.redirect("/");
             return new ModelAndView(map, "hello.handlebars");
 
         }, new HandlebarsTemplateEngine());
